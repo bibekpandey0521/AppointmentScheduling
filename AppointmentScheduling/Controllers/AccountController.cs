8a8a45d16f2968e5b4d38d123ea80fdf75377600
Bibek Pandey <bibek.pandey@dishhome.com.np>
2025-12-14 09:51:51 +0545

    Add project files.

---
+++ AppointmentScheduling/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using AppointmentScheduling.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
+
+namespace AppointmentScheduling.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
